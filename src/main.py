56f17eaa30e7ab3484230ab8c74bd5e837895ba7
Kiyoto Yabe <kiyoto.yabe@ipmu.jp>
2016-08-02 10:45:35 +0900

    20160801

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 import math,os,sys
 import scipy as sp
+import scipy.interpolate as it
+import subprocess
 
-data_dir = './data/'
-pfi_file = ''
+home = '/work/PFS/pfs_obs_sim/'
+data_dir = home + 'data/'
+out_dir = home + 'out/'
+ets_dir = home + 'ets_fiber_assigner/'
+etc_dir = home + 'spt_ExposureTimeCalculator/'
+wave_norm = 480.0
 
 class Pfi():
     def __init__(self,pfi_data_file):
@@ -21,12 +27,29 @@ class Pfi():
         return self.pfi_x_rot,self.pfi_y_rot
 
 class Targets():
-    def __init__(self):
+    def __init__(self,target_file):
+        dat = sp.genfromtxt(target_file,usecols=(2,3,4,17,7,11),dtype=[('id','i4'),('ra','f8'),('dec','f8'),('texp','f8'),('priority','f8'),('mag','f8')])
+        self.tgt_id, self.tgt_x, self.tgt_y, self.tgt_texp, self.tgt_prio, self.tgt_mag = dat['id'], dat['ra'], dat['dec'], dat['texp'], dat['priority'], dat['mag']
+        self.tgt_z = sp.random.uniform(0.5,2.5,len(self.tgt_id))
         return None
-    def get_tgt(self,target_file):
-        dat = sp.genfromtxt(target_file,usecols=(2,3,4),dtype=[('id','S10'),('ra','f8'),('dec','f8')])
-        self.tgt_id, self.tgt_x, self.tgt_y  = dat['id'], dat['ra'], dat['dec']
+    def gen_list_for_ets(self,filename):
+        file = open(filename,'w')
+        for i in range(len(self.tgt_id)):
+            file.write('%10s   %9.5f  %9.5f  %7.1f  %2d  %6.3f  N/A  N/A\n' % ('ID%d'%(self.tgt_id[i]),self.tgt_x[i],self.tgt_y[i],self.tgt_texp[i],self.tgt_prio[i],self.tgt_mag[i]))
+        file.close()
         return 0
+    def get_tgt_dict(self):
+        self.dict_tgt_x = {}
+        self.dict_tgt_y = {}
+        self.dict_tgt_mag = {}
+        self.dict_tgt_z = {}
+        for i in range(len(self.tgt_id)):
+            self.dict_tgt_x[self.tgt_id[i]] = self.tgt_x[i]
+            self.dict_tgt_y[self.tgt_id[i]] = self.tgt_y[i]
+            self.dict_tgt_mag[self.tgt_id[i]] = self.tgt_mag[i]
+            self.dict_tgt_z[self.tgt_id[i]] = self.tgt_z[i]
+        return 0
+
     def get_tgt_id(self):
         return self.tgt_id
     def get_tgt_field_center(self):
@@ -34,24 +57,103 @@ class Targets():
     def map_on_sky(self):
         return self.tgt_x, self.tgt_y
 
-class Assigner():
-    def __init__(self,pfi_x,pfi_y,tgt_x,tgt_x):
+class Template():
+    def __init__(self,filename):
+        self.x, self.y = sp.genfromtxt(filename,unpack=True,usecols=(0,1))
+        intrp = it.interp1d(self.x,self.y)
+        self.mag_norm_temp = intrp(wave_norm)
         return None
-    def get_pfi_map(self):
-        self.pfi_x = pfi_x
-        self.pfi_y = pfi_y
-        return self.pfi_x,self.pfi_y
+    def get_norm_template(self,z,mag,outfile):
+        self.z = z
+        self.mag = mag
+        self.x_norm = self.x * (1+self.z)
+        self.y_norm = self.y + (self.mag - self.mag_norm_temp)
+        file = open(outfile,'w')
+        for i in range(len(self.x)):
+            file.write('%.4e %.4e\n' % (self.x_norm[i],self.y_norm[i]))
+        file.close()
+        return 0
+
+class Ets():
+    def __init__(self):
+        cmd = 'cd %s; make' % (ets_dir)
+        subprocess.call(cmd, shell=True)
+        return None
+    def run_assigner(self,assigner):
+        self.assigner = assigner
+        self.fraction = '0.50'
+        self.objlist = '%s/ets.list' % (out_dir)
+        self.output = '%s/ets.out' % (out_dir)
+        cmd = '%sets_demo assigner=%s input=%s fract=%s output=%s' % (ets_dir,self.assigner,self.objlist,self.fraction,self.output)
+        proc = subprocess.call(cmd, shell=True)
+        if proc == 0:
+            print "Fiber Allocation Done!"
+            C=0
+            self.assigned_tgt = []
+            self.assigned_fib = []
+            self.assigned_tgt_ra = []
+            self.assigned_tgt_dec = []
+            for line in open(self.output,'r'):
+                if C<=1:
+                    a = line.split()
+                    if a[0] == "Exposure":
+                        C+=1
+                    if a[0] != "Exposure" and a[0] != "Target":
+                        self.assigned_tgt.append(int(a[0]))
+                        self.assigned_fib.append(int(a[1]))
+                        self.assigned_tgt_ra.append(float(a[2]))
+                        self.assigned_tgt_dec.append(float(a[3]))
+        else:
+            print "Fiber Allocation Failed!"
+        return 0
+
+class Etc():
+    def __init__(self):
+        cmd = 'cd %s; make' % (etc_dir)
+        proc = subprocess.call(cmd, shell=True)
+        return None
+    def get_sn_table(self):
+        cmd = 'cd %s; python run_etc.py @run_etc.defaults --OUTFILE_NOISE=out/ref.noise.dat --OUTFILE_SNC=out/ref.snc.dat --OUTFILE_SNL=- --EXP_TIME=450 --EXP_NUM=2 --MAG_FILE=23.0 ' % (etc_dir)
+        proc = subprocess.call(cmd, shell=True)
+        if proc == 0:
+            print "SN table is created!"
+        return 0
+    def get_simulated_spectra(self,mag_file,objId):
+        cmd = 'cd %s; python gen_sim_spec.py @gen_sim_spec.defaults --EXP_NUM=2 --MAG_FILE=%s --etcFile=out/ref.snc.dat --nrealize=1 --outDir=%s --asciiTable=None --writeFits=True --objId=%d' % (etc_dir,mag_file,out_dir,objId)
+        proc = subprocess.call(cmd, shell=True)
+        return proc
 
 def main():
-    pfi = Pfi('./data/fiberpos.el90.dat')
-    tgt = Targets()
-    tgt.get_tgt('./data/dbsim_test_cosmology.dat')
-
-    pfi.map_on_sky(pc=(0.0,0.0),pa=30.0)
-    tgt.map_on_sky()
-
-    assgn = Assigner(pfi.pfi_x_rot,pfi.pfi_y_rot,tgt.tgt_x,tgt.tgt_y)
-    assgn.get_pfi_map()
+    if os.path.exists(out_dir)==False:
+        os.mkdir(out_dir)
+#    pfi.map_on_sky(pc=(0.0,0.0),pa=30.0)
+#    tgt.map_on_sky()
+## taget list ##
+    print "loading target list ..."
+    tgt = Targets('%s/catalogue/dbsim_test_cosmology.dat' % (data_dir))
+    tgt.gen_list_for_ets('%s/ets.list' % (out_dir))
+    tgt.get_tgt_dict()
+## ETS package ##
+    print "initializing ETS package ..."
+    ets = Ets()
+    ets.run_assigner(assigner='naive')
+    print "%d objects are assigned!" % (len(ets.assigned_tgt))
+    file = open('test.dat','w')
+    for i in range(len(ets.assigned_tgt)):
+        file.write('%d %.5f %.5f %.5f %.5f\n' % (ets.assigned_tgt[i],ets.assigned_tgt_ra[i],ets.assigned_tgt_dec[i],tgt.dict_tgt_x[ets.assigned_tgt[i]],tgt.dict_tgt_y[ets.assigned_tgt[i]]))
+    file.close()
+## spectral template ##
+    stmp = Template(data_dir+'/spec_template/ex_gal_sf.dat')
+## ETC package ##
+    print "initializing ETC package ..."
+    etc = Etc()
+#    etc.get_sn_table()
+## Simulated spectra ##
+    print "generating simulated spectra ..."
+    for i in range(len(ets.assigned_tgt)):
+        print "ObjId: %d" % (ets.assigned_tgt[i])
+        stmp.get_norm_template(tgt.dict_tgt_z[ets.assigned_tgt[i]],tgt.dict_tgt_mag[ets.assigned_tgt[i]],out_dir+'mag.dat')
+        etc.get_simulated_spectra(mag_file=out_dir+'mag.dat',objId=ets.assigned_tgt[i])
 
 #    print pfi.get_num_cobra()
 #    for i in range(pfi.get_num_cobra()):
